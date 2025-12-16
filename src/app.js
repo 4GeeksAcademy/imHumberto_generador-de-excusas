040da001bba2e6d80ab8f3c341d4a00d62180a6a
Humberto Sánchez <30481822+imHumberto@users.noreply.github.com>
2025-12-16 01:28:04 +0000

    cambios en el JS, funcion de la excusa random

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  console.log("Hello Humberto from the console!")
+  
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'My girlfriend', 'My mom', 'Superman', 'My cat']
+  let action = ['ate', 'peed', 'crushed', 'broke', 'spilled', 'lost', 'burned']
+  let what = ['my homework', 'my phone', 'the car', 'my house', 'the bedroom']
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying']
+  
+  let whoRandom = Math.floor (Math.random() * who.length)
+  let actionRandom = Math.floor(Math.random() * action.length)
+  let whatRandom = Math.floor(Math.random() * what.length)
+  let whenRandom = Math.floor(Math.random() * when.length)
+
+  let mensajeFinal = who[whoRandom] + " " + action[actionRandom] + " " + what[whatRandom] + " " + when[whenRandom]
+
+  console.log(mensajeFinal)
+
+  const mensaje = document.getElementById("excuse")
+
+  mensaje.innerHTML = mensajeFinal
+
+
+}
+
+
+
+
+
