447935047a3824f917ac0a9365ff6d3031be0bd4
Humberto Sánchez <30481822+imHumberto@users.noreply.github.com>
2025-12-18 00:02:09 +0000

    cambios de estilo, optimizacion del JS

--- src/app.js
+++ src/app.js
@@ -13,13 +13,18 @@ window.onload = function() {
   let action = ['ate', 'peed', 'crushed', 'broke', 'spilled', 'lost', 'burned']
   let what = ['my homework', 'my phone', 'the car', 'my house', 'the bedroom']
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying']
-  
-  let whoRandom = Math.floor (Math.random() * who.length)
-  let actionRandom = Math.floor(Math.random() * action.length)
-  let whatRandom = Math.floor(Math.random() * what.length)
-  let whenRandom = Math.floor(Math.random() * when.length)
 
-  let mensajeFinal = who[whoRandom] + " " + action[actionRandom] + " " + what[whatRandom] + " " + when[whenRandom]
+  function randomRandom (array){
+    let resultado = Math.floor(Math.random() * array.length)
+    return resultado
+  }
+  
+  //let whoRandom = Math.floor (Math.random() * who.length)
+  //let actionRandom = Math.floor(Math.random() * action.length)
+  //let whatRandom = Math.floor(Math.random() * what.length)
+  //let whenRandom = Math.floor(Math.random() * when.length)
+
+  let mensajeFinal = who[randomRandom(who)] + " " + action[randomRandom(action)] + " " + what[randomRandom(what)] + " " + when[randomRandom(when)]
 
   console.log(mensajeFinal)
 
